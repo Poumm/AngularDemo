a4229710d81ddd0b9866b966f84dc882bd3d985b
Guillaume Scheibel <guillaume.scheibel@gmail.com>
2013-01-30 16:49:59 +0100

    Initial commit

---
+++ client/src/main/webapp/js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var app = angular.module('app', []);
+
+app.controller('listController', function listController($scope) {
+    $scope.predicate = 'iso';
+    $scope.reverse = 'false';
+    $scope.countries = [
+        {iso: 'FR', name: 'France'},
+        {iso: 'US', name: 'Etats-Unis'}
+    ];
+
+    $scope.edit = function (client) {
+        $scope.currentClient = client;
+    }
+});
+
+app.filter('wikipedia', function () {
+    return function (countryName) {
+        return 'http://fr.wikipedia.org/wiki/' + countryName;
+    }
+});
